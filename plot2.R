acacfc066f3ef598e23fc84aee0aff53f0d1721d
AllanBrunner <allan_brunner@icloud.com>
2021-03-08 18:47:08 -0700

    Changes to plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 ########1#########2#########3#########4#########5#########6#########7###########
 #                                                                              #
-#  plot1.R (03/8/2021)                                                         #
+#  plot2.R (03/8/2021)                                                         #
 #                                                                              #
-#  This program creates plot #1 for the EDA Assignment #1.                     #
+#  This program creates plot #2 for the EDA Assignment #1.                     #
 #                                                                              #
 #########1#########2#########3#########4#########5#########6#########7##########
 
@@ -10,25 +10,29 @@
 
 
 #  read in household power consuption data
-# keep only observations for 2007-02-01 and 2007-02-02
 
    hpc_dat <- read.table("data/hpc_keep.txt", stringsAsFactors=FALSE, header=TRUE)
    
    hpc_dat$Date <- as.Date(hpc_dat$Date2, format="%d/%m/%Y")
    
+   hpc_dat$DateTime <- paste0(hpc_dat$Date, "", hpc_dat$Time)
    
+   hpc_dat$DateTime<- as.POSIXct(hpc_dat$DateTime, format="%Y-%m-%d %H:%M:%S")
    
    head(hpc_dat)
    summary(hpc_dat)
 
    
-#  creat 2n plot -- a historgram of global active power 
+#  creat 2nd plot -- a time-series plot of of global active power 
 #  save plot in png format   
    
    
-   plot(hpc_dat$Date, hpc_dat$Global_active_power) 
+   ggplot(hpc_dat, aes(x=DateTime, y=Global_active_power)) + 
+      geom_line() +
+      labs(x="", y="Global Active Power (in kilowatts)",
+           title="Global Active Power: Febrary 1-2, 2007")
         
-   dev.copy(png, file="plot1.png")
+   dev.copy(png, file="plot2.png")
    dev.off()   
    
    
